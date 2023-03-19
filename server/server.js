2e878909fbf8d25bdc1f799305ffa6d20dc1ce88
arjun4619 <sun4shocker@gmail.com>
2023-03-19 13:55:11 +0530

    update changes

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.get("/", async (req, res) => {
   res.status(200).send({
-    message: "Hello from Codex",
+    message: "Hello from CodeX!",
   });
 });
 
@@ -29,20 +29,20 @@ app.post("/", async (req, res) => {
       model: "text-davinci-003",
       prompt: `${prompt}`,
       temperature: 0,
-      max_tokens: 300,
+      max_tokens: 3000,
       top_p: 1,
-      frequency_penalty: 0.5,
       presence_penalty: 0,
     });
+
     res.status(200).send({
       bot: response.data.choices[0].text,
     });
   } catch (error) {
-    console.log(error);
-    res.status(500).send({ error });
+    console.error(error);
+    res.status(500).send(error || "Something went wrong");
   }
 });
 
 app.listen(5000, () =>
-  console.log(`Server is running on port http://localhost:5000`)
+  console.log("AI server started on http://localhost:5000")
 );
